c567d66b547052f320ac816f5545237416f3f674
Minh Vu <minhd_vu@yahoo.com>
2021-11-04 22:52:37 -0400

    added enemy back and forth movement

--- move_enemy.lua
+++ move_enemy.lua
@@ -1,7 +1,17 @@
-move_enemy(
-    10, 20,
-    11, 22,
-    10, 20,
-    30, 40,
-    50, 60
-);
+math.randomseed(os.time())
+
+positions = {}
+
+for y = 10, 40, 2 do
+    for x = 10, 40 do        
+        table.insert(positions, x)
+        table.insert(positions, y)
+    end
+
+    for x = 40, 10, -1 do
+        table.insert(positions, x)
+        table.insert(positions, y + 1)
+    end
+end
+
+move_enemy(unpack(positions));
